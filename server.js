8c9d6da010bf97ce5427acf870b4adf88314dbef
geo-studios-team <geo.studios.team@gmail.com>
2025-06-28 02:41:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,17 @@ const deviceRoutes = require("./routes/devices");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/towers", towerRoutes);
 app.use("/api/devices", deviceRoutes);
 
-// DB Connect & Server Start
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error("MongoDB error:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
